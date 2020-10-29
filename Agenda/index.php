c428a9b25a2291e969931816219060d321448013
DaBI <daniel.itomarim@gmail.com>
2020-10-29 10:44:56 -0300

    organizei as pastas, to no inicio ainda

---
+++ Agenda/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda Impacto</title>
+</head>
+<body>
+      <header>
+        <h1>Agenda de Contatos</h1>
+          <nav>
+          <a href="index.php?menuop=home">Home</a> |
+          <a href="index.php?menuop=contatos">Contatos</a> |
+          <a href="index.php?menuop=eventos">Eventos </a> |
+          <a href="index.php?menuop=sobre">Sobre</a> |
+          </nav>
+      </header>
+      <main>
+          <?php
+               $menuop =(isset($_GET["menuop"]))?$_GET["menuop"]:"home";
+               switch ($menuop) {
+                 case 'home':
+                    include ("paginas/home/home.php");
+                   break;
+
+                 case 'contatos':
+                    include ("paginas/contatos/contatos.php");
+                   break;
+
+                 case 'eventos':
+                  include ("paginas/eventos/eventos.php");
+                   break;
+
+                 case 'sobre':
+                  include ("paginas/sobre/sobre.php");
+                   break; 
+                   
+                 default:
+                 include ("paginas/home/home.php");
+                   break;
+               }
+          
+            
+          ?>
+      </main>
+</body>    
+</html>
